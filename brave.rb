1a2caf5fbcd8b73aa7406bdd267550856c24baac
keisuke <keisukent0228@i.softbank.jp>
2020-12-27 14:09:11 +0900

    三項演算子を後置ifに変更

--- brave.rb
+++ brave.rb
@@ -3,7 +3,7 @@ class Brave < Character
   def attack(monster)
     damage = @offense - monster.defense / 2
     monster.hp -= damage
-    monster.hp <= 0 ? monster.hp = 0 : monster.hp
+    monster.hp = 0 if monster.hp <= 0
     puts "#{@name}の攻撃!"
     puts "#{monster.name}に #{damage} のダメージを与えた!"
   end
--- monster.rb
+++ monster.rb
@@ -3,7 +3,7 @@ class Monster < Character
   def attack(brave)
     damage = @offense - brave.defense / 2
     brave.hp -= damage
-    brave.hp <= 0 ? brave.hp = 0 : brave.hp
+    brave.hp = 0 if brave.hp <= 0
     puts "#{@name}の攻撃!"
     puts "#{brave.name}は #{damage} のダメージを受けた!"
   end
